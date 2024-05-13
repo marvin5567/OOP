4aee45f9ff156c4d3fcdd4065e45e1b967ce3492
marvin5567 <minicreepergaming@gmail.com>
2024-05-13 04:33:08 +0300

    oops forgot the comments

--- item.py
+++ item.py
@@ -5,16 +5,17 @@ class Item:
         self.name = name
         self.desc = desc
         self.itemType = itemType
-        self.isItemTypeValid()
+        self.checkItemType()
             
 
-    def isItemTypeValid(self):
-        if self.itemType.upper() not in itemTypes:
-            raise invalidItemType
+    def checkItemType(self):
+        if self.itemType.upper() not in itemTypes: # is it valid?
+            raise invalidItemType # if not, raise exception
         
         else:
-            exec(f'self.create{self.itemType.upper()}()')
+            exec(f'self.create{self.itemType.upper()}()') # cool function that executes a function as a string
 
+    # maybe could be classes?
     def createFOOD(self):
         pass
     def createWEAPON(self):
