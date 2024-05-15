b49a4ebd271f2ace41b56f93ba080d49f21cf356
marvin5567 <minicreepergaming@gmail.com>
2024-05-15 15:54:45 +0300

    wrong branch!!! should've been in player branch, but atm working on the health system alongside main application; some beta testing in the works!

--- player.py
+++ player.py
@@ -1,10 +1,26 @@
 class Player:
-    def __init__(self, name: str, health: int, dmg: int, defense: int, isDead: bool):
+    def __init__(self, name: str, maxHealth:int, health: int, maxHunger: int, hunger: int, dmg: int, defense: int, isDead: bool):
         self.name = name
         self.health = health
         self.dmg = dmg
         self.defense = defense
+        self.maxHealth = maxHealth
+        self.isDead = isDead
+        self.maxHunger = maxHunger
+        self.hunger = hunger
+        
+    def healthCheck(self):
+        if self.health <= 0:
+            self.isDead == True
+            print("You died!!!")
+            return self.isDead
     
+    def healthChange(self, incomingHealthChange):
+        if self.maxHealth <= self.health + incomingHealthChange:
+            self.health == self.maxHealth
+            print(f"HP: {self.health}/{self.maxHealth}")
+        else:
+            self.health += incomingHealthChange
+            print(f"HP: {self.health}/{self.maxHealth}")
     
-    
-    
+        self.healthCheck()
